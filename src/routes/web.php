4d6ee6ba85c4d2d7e0f4574e3cc1f490bc3998cf
Dominic <developer@osit.co.za>
2023-11-22 15:51:04 +0200

    Refactored routes.

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,4 +3,6 @@
 use Osit\Webalitics\Controllers\WebaliticController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('webalitics/admin', [WebaliticController::class, "adminWebalitic"]);
+Route::get('/webalitics/admin', [WebaliticController::class, "adminWebalitic"]);
+
+Route::post('/webalitics/webalitic-profile', [WebaliticController::class, "adminWebaliticProfile"])->name('/webalitics/webalitic-profile');
