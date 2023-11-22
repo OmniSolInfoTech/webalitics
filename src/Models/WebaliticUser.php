fb43203bcb01bb497a1301e7a81bdeda728f636b
Dominic <developer@osit.co.za>
2023-11-22 15:23:42 +0200

    WebaliticUser model.

---
+++ src/Models/WebaliticUser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Osit\Webalitics\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class WebaliticUser extends Model
+{
+    protected $table = 'webalitic_user';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_name',
+        'website_name',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+    ];
+}
