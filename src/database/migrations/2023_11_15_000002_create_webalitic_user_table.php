0b321494518e7090c1d4c5e1d2681485445457f3
Dominic <developer@osit.co.za>
2023-11-22 15:19:53 +0200

    Migration file for table Webalitic_User .

---
+++ src/database/migrations/2023_11_15_000002_create_webalitic_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use \Illuminate\Database\Migrations\Migration;
+use \Illuminate\Support\Facades\Schema;
+use \Illuminate\Database\Schema\Blueprint;
+
+class CreateWebaliticUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("webalitic_user", function(Blueprint $table){
+            $table->integer("id")->autoIncrement();
+            $table->string("user_name")->default("User 123");
+            $table->string("website_name")->default("Website 123");
+            $table->integer("successful_transactions")->default(0);
+            $table->integer("failed_transactions")->default(0);
+            $table->tinyInteger("credentials_faulty")->default(0);
+            $table->timestamp("created_at")->useCurrent();
+            $table->timestamp("updated_at")->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('webalitic_user');
+    }
+}
