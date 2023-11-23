da7ac075ea52fa971816a38a5abf3209053142bf
Dominic <developer@osit.co.za>
2023-11-23 08:11:03 +0200

    Dynamic page title.

--- src/resources/views/boilerplate.blade.php
+++ src/resources/views/boilerplate.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>WEBALITICS Boilerplate</title>
+        <title>@yield('title')</title>
         <link rel="stylesheet" href="{{ asset('webalitic-assets/style.css') }}">
         <!-- JQuery -->
         <script src="{{ asset('webalitic-assets/jquery-3.7.1.min.js') }}"></script>
